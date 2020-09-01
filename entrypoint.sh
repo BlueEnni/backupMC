535f65aaa73307f44bcee752c2e58f9b8026bd18
BlueEnni <Enrico@bachus.it>
2020-09-01 18:28:58 +0200

    fixed the echo command

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,9 @@ cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 apk del tzdata
 #Add the backupcount
 touch /files/backup_data_MC_new.sh
-{ echo -n "#!/bin/bash\n"; echo -n"BACKUPCOUNT="$BACKUPCOUNT"\n"; cat /files/backup_data_MC_dyn.sh; } > /files/backup_data_MC_new.sh
+echo -n "#!/bin/bash" > /files/backup_data_MC_new.sh
+echo -n"BACKUPCOUNT="$BACKUPCOUNT"" >> /files/backup_data_MC_new.sh
+cat /files/backup_data_MC_dyn.sh >> /files/backup_data_MC_new.sh
 # Add the cronjobs
 echo "${BACKUPDENSITYCRON}/files/backup_data_MC_new.sh" > /etc/crontabs/root
 # Start the crond process

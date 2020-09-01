08a5b0de90cefebebeeec11ac9f440d824155821
BlueEnni <Enrico@bachus.it>
2020-09-01 19:07:54 +0200

    added x writes to the backupscript

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ touch /files/backup_data_MC_new.sh
 echo "#!/bin/bash" > /files/backup_data_MC_new.sh
 echo "BACKUPCOUNT="$BACKUPCOUNT"" >> /files/backup_data_MC_new.sh
 cat /files/backup_data_MC_dyn.sh >> /files/backup_data_MC_new.sh
+chmod +x /files/backup_data_MC_new.sh
 # Add the cronjobs
 echo "${BACKUPDENSITYCRON}/files/backup_data_MC_new.sh" > /etc/crontabs/root
 # Start the crond process
